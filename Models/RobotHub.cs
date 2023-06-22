bfccce38153d8f366e79a847d74c2c6b25b98a79
Ciro Pineda <cpineda@q10.com>
2023-06-22 14:54:57 -0500

    first commit

---
+++ Models/RobotHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace BattleRobot.Models
+{
+    public class RobotHub : Hub
+    {
+        public void SendRobotInformation(string robot)
+        {
+            // Envía la notificación a todos los clientes excepto al que envió el mensaje
+            Clients.Others.ReceiveNotification(robot);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+
+namespace BattleRobot
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var signalRConnection = "Endpoint=https://signalr-jackgold.service.signalr.net;AccessKey=DcZaHo9RjLLm6+3HLnPedcCPI7A9I6idaPfhIaXCRHM=;Version=1.0;";
+            app.MapAzureSignalR("RobotWar", options =>
+            {
+                options.ConnectionString = signalRConnection;
+            });
+
+        }
+    }
+}
